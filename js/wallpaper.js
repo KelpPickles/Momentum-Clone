0759db47ffe9ca53de1c49b9d90bcb223a1a04fe
Min-Gyu Kim <45328809+KelpPickles@users.noreply.github.com>
2024-05-14 16:19:06 +0900

    Update wallpaper.js

--- js/wallpaper.js
+++ js/wallpaper.js
@@ -4,4 +4,4 @@ const cont = document.getElementById("container");
 const wplim = 5;
 const randn = rand(1,wplim);
 
-cont.setAttribute('style', `background-image : url('../wallpaper/${randn}.jpg')`);
+cont.setAttribute('style', `background-image : url('wallpaper/${randn}.jpg')`);
